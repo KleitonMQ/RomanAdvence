b3ac256a094ba7a5ccd8585908c25b122e9db3ad
Kleiton MQ <kleiton.x5@gmail.com>
2023-03-17 17:44:19 -0300

    configuracao de camera para percorrer o mapa junto com o player.

---
+++ Assets/Scripts/Cam.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    private Transform player;
+    public float smooth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 following;
+        following = new Vector3(player.position.x, transform.position.y, transform.position.z);
+        if (transform.position.x >= 0)
+        {
+            //following = new Vector3(player.position.x, transform.position.y, player.position.z);
+            Debug.Log(player);
+            //O Vector3.Lerp, é uma forma de seguir um ponto de forma menos brusca. Dessa forma, ele recebe 3 parametros, um da posição inicial do movimento, um da posical que ele deveria chegar e por ultimo o tempo que ele deveria levar pra acompanhar.
+            transform.position = Vector3.Lerp(transform.position, following, smooth * Time.deltaTime);
+        }
+        if (transform.position.x < 0)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(0f,transform.position.y,transform.position.z), smooth);
+        }
+
+
+    }
+}
